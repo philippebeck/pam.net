1f9a07b47a93900d5821c3e184af4251dbd1f689
Philippe Beck <philippe@philippebeck.net>
2020-06-27 03:04:47 +0200

    Create AdminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Pam\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ */
+class AdminController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        if ($this->service->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        $classes    = ModelFactory::getModel("Class")->listData();
+        $properties = ModelFactory::getModel("Property")->listData();
+        $methods    = ModelFactory::getModel("Method")->listData();
+        $users      = ModelFactory::getModel("User")->listData();
+
+        return $this->render("admin/admin.twig", [
+            "classes"       => $classes,
+            "properties"    => $properties,
+            "methods"       => $methods,
+            "users"         => $users
+        ]);
+    }
+}
